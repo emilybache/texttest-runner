fc5ba0c1ef5ba43d0c41ca4d4f9f665c20b49203
Emily Bache <emily@bacheconsulting.com>
2014-03-12 21:50:36 +0100

    Added a java class to make it easier to generate javadoc and sources jars.

---
+++ src/main/java/Hello.java
@@ -0,0 +1,10 @@
+
+/**
+ * This is the maven repo for the TextTest Runner. There are no Java sources and hence no javadoc.
+ * Please see http://texttest.org for documentation.
+ */
+public class Hello {
+	public static void main(String[] args) {
+		System.out.println("Hello! This is the maven repo for the TextTest Runner. There are no Java sources and hence no javadoc.");
+	}
+}
